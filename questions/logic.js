b98cf537027dd79d13697fa4c8e4e80b506839e8
ARUN PAL <arunpal6390@gmail.com>
2025-08-15 00:12:27 +0530

    new file added

--- questions/logic.js
+++ questions/logic.js
@@ -1,1 +1,32 @@
 console.log("Hello");
+// let number = 5;
+// if (number == 1) {
+//   console.log("A");
+// } else if (number == 2) {
+//   console.log("B");
+// } else {
+//   console.log("F");
+// }
+
+// let marks = 20;
+// switch (marks) {
+//   case 1:
+//     console.log("A");
+//     break;
+
+//   case 2:
+//     console.log("B");
+//     break;
+//   default:
+//     console.log("F");
+// }
+// let n = prompt("Enter number how many times you want to print Good night:");
+// let n = " ";
+// for (let i = 1; i < 9; i++) {
+//   n += i + " ";
+// }
+// console.log(n);
+
+for (let i = 0; i < 5; i++) {
+  console.log("*");
+}
