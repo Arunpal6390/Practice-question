297a8a6906c20e8abb72d68e8b767af62cda572c
ARUN PAL <arunpal6390@gmail.com>
2025-08-19 16:42:04 +0530

    new learning added

--- questions/logic.js
+++ questions/logic.js
@@ -1,4 +1,4 @@
-console.log("Hello");
+// console.log("Hello");
 // let number = 5;
 // if (number == 1) {
 //   console.log("A");
@@ -27,6 +27,68 @@ console.log("Hello");
 // }
 // console.log(n);
 
-for (let i = 0; i < 5; i++) {
-  console.log("*");
+// for (let i = 0; i < 5; i++) {
+
+// }
+
+// function definition
+// function printNumber(num) {
+//   for (let i = 0; i < 10; i++) {
+//     console.log(i + num);
+//   }
+// }
+// printNumber(8);// function call,without function call we cant't execute the function
+
+function printEvenNumber(given_number) {
+  if (given_number % 2 == 0) {
+    console.log("Even Number");
+  } else {
+    console.log("Odd Number");
+  }
 }
+
+function checkVoter(age) {
+  if (age >= 18) {
+    console.log("eligible for voter");
+  } else {
+    console.log("Not eligible for vote");
+  }
+}
+
+function check_given_number_multipleFive(num) {
+  if (num % 5 == 0) {
+    console.log(`${num} is multiple of five`);
+  } else {
+    console.log(`${num} is not multiple of five`);
+  }
+}
+
+function printEvenNumbers() {
+  let result = "";
+  for (let i = 0; i < 100; i++) {
+    if (i % 2 == 0) {
+      // console.log(`Even numbers: ${i}`); //templet literals used for print string as well as values.
+      result = result + i + ",";
+    }
+  }
+  console.log(result);
+}
+
+function printOddNumbers() {
+  total = 0;
+  cout = 0;
+  for (let i = 0; i <= 5; i++) {
+    if (i % 2 !== 0) {
+      total = total + i;
+      cout++;
+    }
+  }
+  console.log("1 to 100 odd number sum:- " + total);
+  console.log("1 to 100 between odd numbe :- " + cout);
+}
+
+printEvenNumber(7.5);
+checkVoter(71);
+check_given_number_multipleFive(5);
+printEvenNumbers();
+printOddNumbers();
